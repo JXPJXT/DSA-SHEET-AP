79fd2a68c8d7244255550a86f56a8028acc1e3ae
JXPJXT <samjj317@gmail.com>
2025-08-08 11:50:28 +0530

    v6

---
+++ 1.Arrays/Easy/SearchInRotSortedQ6.java
@@ -0,0 +1,45 @@
+// Ques-6
+// Platform: LeetCode
+// Companies: Microsoft, Google, Adobe, Amazon, D-E-Shaw, Flipkart, Hike, Intuit, MakeMyTrip, Paytm
+// Time Complexity: O(log n) - Binary search in rotated sorted array
+// Space Complexity: O(1) - Constant extra space
+
+public class SearchInRotSortedQ6 {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int result = search(nums, target);
+        System.out.println("Index of target " + target + " is: " + result);
+
+        int missingTarget = 3;
+        int missingResult = search(nums, missingTarget);
+        System.out.println("Index of target " + missingTarget + " is: " + missingResult);
+    }
+
+    public static int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            final int m = (l + r) / 2;
+            if (nums[m] == target)
+                return m;
+
+            if (nums[l] <= nums[m]) { 
+                if (nums[l] <= target && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            } else { 
+                if (nums[m] < target && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+        }
+
+        return -1;
+    }
+}
