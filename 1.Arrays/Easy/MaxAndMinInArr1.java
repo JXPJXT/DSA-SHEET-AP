bb384253fd289d7d7cb7359998e0ddcf41f9bd1e
JXPJXT <samjj317@gmail.com>
2025-08-08 09:46:42 +0530

    v1

---
+++ 1.Arrays/Easy/MaxAndMinInArr1.java
@@ -0,0 +1,47 @@
+//Ques 1
+//Platform GFG
+//Company: ABCO Accolite Amazon Cisco Hike Microsoft Snapdeal VMWare Google Adobe
+import java.util.*;
+
+class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
+public class MaxAndMinInArr1 {
+    public static void main(String[] args) {
+        int[] arr = {1000, 11, 445, 1, 330, 3000};
+        Pair<Integer, Integer> result = getMinMax(arr);
+        System.out.println(result.getKey() + " " + result.getValue());
+    }
+
+    static Pair<Integer, Integer> getMinMax(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new Pair<>(Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+        return new Pair<>(max, min);
+    }
+}
